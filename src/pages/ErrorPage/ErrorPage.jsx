652f7e260fbadf8191c8f3d124c62f47d6053db5
Ahmad Raiyan <ahmadraiyanofficial@gmail.com>
2026-03-14 17:57:39 +0600

    created navbar & organized structure

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div className='flex justify-center items-center h-screen font-bold text-5xl bg-teal-500 text-white'>
+            <p>404 | Page Not Found</p>
+        </div>
+    );
+};
+
+export default ErrorPage;
