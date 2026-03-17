ab4288c1bc9827cfe68a9d5ed5c7222d7da7cc75
Ahmad Raiyan <ahmadraiyanofficial@gmail.com>
2026-03-17 12:37:50 +0600

    implemented sorting functionality

--- src/pages/ListedBooks/ListedBooks.jsx
+++ src/pages/ListedBooks/ListedBooks.jsx
@@ -4,9 +4,11 @@ import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import { getStoredBook } from "../../utilities/addToDB";
 import ReadBook from "../ReadBook/ReadBook";
+import { FaChevronDown } from "react-icons/fa";
 
 const ListedBooks = () => {
   const [readBooks, setReadBooks] = useState([]);
+  const [sort, setSort] = useState("");
   const bookData = useLoaderData();
   useEffect(() => {
     const readBooksData = getStoredBook().map((id) => parseInt(id));
@@ -15,11 +17,44 @@ const ListedBooks = () => {
     );
     setReadBooks(books);
   }, [bookData]);
+  const handleSort = (type) => {
+    if (type === "Pages") {
+      setSort(type);
+      const sortedByPages = [...readBooks].sort(
+        (a, b) => a.totalPages - b.totalPages,
+      );
+      setReadBooks(sortedByPages);
+    }
+    if (type === "Ratings") {
+      setSort(type);
+      const sortedByPages = [...readBooks].sort((a, b) => a.rating - b.rating);
+      setReadBooks(sortedByPages);
+    }
+  };
   return (
     <div>
       <h2 className="text-center font-bold text-2xl bg-gray-200 py-10 my-4 rounded-lg">
         Books
       </h2>
+      <div className="text-center">
+        <div className="dropdown">
+        <div tabIndex={0} role="button" className="btn m-1 btn-success">
+          Sort By: {sort ? sort : ""} <FaChevronDown />
+        </div>
+        <ul
+          tabIndex="-1"
+          className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+        >
+          <li>
+              <a onClick={() => handleSort("Pages")}>Pages</a>
+            </li>
+            <li>
+              <a onClick={() => handleSort("Ratings")}>Ratings</a>
+            </li>
+        </ul>
+      </div>
+      </div>
+      
       <div className="my-6">
         <Tabs>
           <TabList>
